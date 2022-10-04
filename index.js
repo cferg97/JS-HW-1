cdecc3a23437c681961cc6ba7d06260f3f8ee5f0
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-10-04 13:05:33 +0100

    Created file
    Assigned values and printed to console

---
+++ index.js
@@ -0,0 +1,11 @@
+let firstName = "Catriona"
+let middleName = "Mary"
+let lastName = "Ferguson"
+let fullName = firstName + " " + middleName + " " + lastName 
+console.log("Hello, my name is " + fullName)
+
+let myAge = 25
+let currentYear = 2022
+let birthYear = currentYear - myAge
+console.log("I was born in the year "+ birthYear)
+
